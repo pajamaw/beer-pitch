281a594c80ff7cc7def350fce82f1d43e10c7bb7
pajamaw <pjpeterww@gmail.com>
2016-06-28 16:40:33 -0400

    added current_user to the scope, currently adjusting event resolves in order to use dynamic oauth_token. will be adding api call to individual event next in order to grab the owner object

--- app/assets/javascripts/ang/controllers/MainController.js
+++ app/assets/javascripts/ang/controllers/MainController.js
@@ -1,16 +1,39 @@
 app.controller('MainController', MainController)
 function MainController($scope, $timeout, Facebook, User){
   $scope.events = {};
-  var current_user = User.get({id: 1})
-
-  $scope.eve = function() {
-    Facebook.api('/me/events?access_token=EAAPdcktnZCWsBAJQzp2PorKBZA7x1LqszbbmUcHpZBoEs9XI1w1ZCZCRu0dMpjJUu9VLQvizmZBFOQ7mZCbAPVPqQMt2njX2Or0vA6nZBDwNgm1IgUlzB8OLX9Cpd1NfNsdZCPsgkFYKgaW7u0oLdDHkqySbKKfDlifoZD&since=today&limit=200',
+  $scope.euser = {};
+  var current_user;
+  var url;
+  var oauth;
+  User.get({id: '1'}), function(data){
+    $scope.current_user = data;
+  };
+  //oauth = current_user.user.oauth_token;
+  url = '/me/events?access_token='+ oauth+ '&since=today&limit=200';
+  $scope.events = function() {
+    debugger;
+    //EAAPdcktnZCWsBAJQzp2PorKBZA7x1LqszbbmUcHpZBoEs9XI1w1ZCZCRu0dMpjJUu9VLQvizmZBFOQ7mZCbAPVPqQMt2njX2Or0vA6nZBDwNgm1IgUlzB8OLX9Cpd1NfNsdZCPsgkFYKgaW7u0oLdDHkqySbKKfDlifoZD
+    //$scope.current_user = User.get({id: 1});
+    //change to url once i get it to set
+    Facebook.api('/me/events?access_token=EAAPdcktnZCWsBAJQzp2PorKBZA7x1LqszbbmUcHpZBoEs9XI1w1ZCZCRu0dMpjJUu9VLQvizmZBFOQ7mZCbAPVPqQMt2njX2Or0vA6nZBDwNgm1IgUlzB8OLX9Cpd1NfNsdZCPsgkFYKgaW7u0oLdDHkqySbKKfDlifoZD&since=today&limit=200',//url,
       function(response){
         $scope.$apply(function(){
           $scope.events = response.data;
           debugger;
         });
-      }
-  )}
-  $scope.eve();
+      })
+    //  var event_id = $scope.events.
+};
+$scope.euser = function(){
+  Facebook.api('/468029196741743',
+    function(response){
+      $scope.$apply(function(){
+        $scope.euser = response.data;
+        debugger;
+    });
+  })
+};
+$scope.euser();
+  $scope.events();
+  debugger;
 }
--- app/assets/javascripts/ang/services/UserService.js
+++ app/assets/javascripts/ang/services/UserService.js
@@ -1,11 +1,11 @@
-app.factory('User', User);
+app.factory('User', User)
 
 function User($resource){
 
-  var User = $resource('/api/user/:id', {id: 'id'}, {
+  var User = $resource('/api/user/:id:.format', {id: '@id'}, {
     get: {method: 'GET'},
     update: {method: 'PUT'},
     delete: {method: 'DELETE'}
-  });
+  })
   return User;
 }
